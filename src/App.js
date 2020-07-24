8c85df7f5fae8202f8c8f7861e748c85bd4cdb6e
Jamie Grantham-Burton <jamie.granthamburton@zebra.com>
2020-07-24 11:14:59 +0100

    Set up basic view for UserTable.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
+import UserTable from './tables/UserTable';
 
 const App = () => {
+
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ];
+
+  const [users, setUsers] = useState(usersData);
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -10,10 +20,11 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable users={users}/>
         </div>
       </div>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/tables/UserTable.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const UserTable = ({ users }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {users.length > 0 ? (
+        users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>
+              <button className="button muted-button">Edit</button>
+              <button className="button muted-button">Delete</button>
+            </td>
+          </tr>
+        ))
+      ) :
+        (
+          <tr>
+            <td colSpan={3}>No users</td>
+          </tr>
+        )
+      }
+
+    </tbody>
+  </table>
+)
+
+export default UserTable;
